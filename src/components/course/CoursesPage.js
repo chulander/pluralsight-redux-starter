10da98482eb769ccbb646d676f396556e0618a04
Bryan Chu <bryan@Bryans-MacBook-Pro.local>
2016-09-18 17:56:52 -0400

    completed up to video 79

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,44 +1,68 @@
 /* eslint-disable */
 
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
 /* eslint-enable */
 
-class CoursesPage extends React.Component {
-  constructor(props,context){
-    super(props,context);
+import * as courseActions from '../../actions/courseActions';
+
+class CoursesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
     this.state = {
-      course: { title: ''}
+      course: {title: ''}
     };
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onClickSave = this.onClickSave.bind(this);
+    this.courseRow = this.courseRow.bind(this);
   }
 
-  onTitleChange(event){
+  onTitleChange(event) {
     const course = this.state.course;
     course.title = event.target.value;
     this.setState({
       course: course
     });
   }
-  onClickSave(){
-   alert(`Saving ${this.state.course.title}`);
+
+  onClickSave() {
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   }
-  render(){
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
           onChange={this.onTitleChange}
-          value={this.state.course.title} />
+          value={this.state.course.title}/>
         <input
           type="submit"
           value="Save"
-          onCLick={this.onClickSave} />
+          onClick={this.onClickSave}/>
       </div>
     );
   }
 }
+CoursesPage.propTypes= {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+  "use strict";
+  // state.courses, the "courses" came from the rootReducers' return property
+  // refer to reducers/index.js
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
