2df06933a5bf1874bd1134bdce5eb3ee4d4d3f7d
Bryan Chu <bryan@Bryans-MacBook-Pro.local>
2017-02-13 03:57:27 -0500

    finished Redux Flow chap 81

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import 'babel-polyfill'
-import React from 'react'
-import {render} from 'react-dom'
-import {Router, browserHistory} from 'react-router'
-import routes from './routes'
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
 import './styles/styles.css'; //Webpack can import CSSfiles too
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
 
+const store = configureStore();
 render(
-  <Router history={browserHistory} routes={routes} />,
-  document.getElementById('app')
-)
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
+document.getElementById('app')
+);
