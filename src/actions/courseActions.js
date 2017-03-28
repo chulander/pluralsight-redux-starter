882eca027332ca7688db601aeb90577139700641
Bryan Chu <bryan@Bryans-MacBook-Pro.local>
2017-03-28 03:40:32 -0400

    chapter 119

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,7 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
+
 export function loadCoursesSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
@@ -22,6 +24,7 @@ export function createCourseSuccess(course){
 }
 export function loadCourses(){
   return function(dispatch){
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses=>{
       dispatch(loadCoursesSuccess(courses));
     })
@@ -32,6 +35,7 @@ export function loadCourses(){
 }
 export function saveCourse(course){
   return function(dispatch, getState){
+    dispatch(beginAjaxCall());
     return courseApi.saveCourse(course).then(course=>{
       course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course))
     })
